e89674db654c8468985c4788dfcfbddc7902f3ba
ayash <kinantilaras128@gmail.com>
2023-10-09 21:35:28 +0700

    first commit

---
+++ Modul 1.2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int pjg, lbr;
+	pjg = 10;
+	lbr = 7;
+	
+	int luas = pjg * lbr;
+	
+	printf("%i", luas);
+	return 0;
+}
---
+++ Modul 2.3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int alas, tinggi;
+	float phytagoras; alas = 4, tinggi = 5;
+	
+	int sm = (alas*alas)+(tinggi*tinggi);
+	
+	printf("sm");
+	printf("\nAlas = %i", alas);
+	printf("\nTinggi = %i", tinggi);
+	printf("\nsm = %i", sm);
+	
+	return 0;
+}
